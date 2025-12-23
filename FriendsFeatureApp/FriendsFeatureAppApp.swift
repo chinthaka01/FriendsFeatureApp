98655301feae0ff035577d938a61eae5b0c7e9bb
chinthaka01 <chinthaka01@gmail.com>
2025-12-22 21:31:56 -0500

    Initial Commit.

---
+++ FriendsFeatureApp/FriendsFeatureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFeatureAppApp.swift
+//  FriendsFeatureApp
+//
+//  Created by Chinthaka Perera on 12/22/25.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFeatureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FriendsFeatureAppTests/FriendsFeatureAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFeatureAppTests.swift
+//  FriendsFeatureAppTests
+//
+//  Created by Chinthaka Perera on 12/22/25.
+//
+
+import Testing
+@testable import FriendsFeatureApp
+
+struct FriendsFeatureAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
